fda1a44fc6489c3b047e25d2b73838f7c8d5b2dc
Laurin Hohbach <laurinhohbach@web.de>
2024-02-27 20:08:04 +0100

    still buggy, do not reload

--- Server_StartGame.lua
+++ Server_StartGame.lua
@@ -6,17 +6,15 @@ require("SetOwnedCapital");
 ---@param game GameServerHook
 ---@param standing GameStanding
 function Server_StartGame(game, standing)
-
     local PlayerGameData = Mod.PlayerGameData;
 
     for playerid, _ in pairs(game.Game.PlayingPlayers) do
-            PlayerGameData[playerid] = {};
+        PlayerGameData[playerid] = {};
     end
 
     ---@type table
     local PrivateGameData = Mod.PrivateGameData;
-	if not Mod.Settings.PlayersChooseCapitalLocation then
-
+    if not Mod.Settings.PlayersChooseCapitalLocation then
         ---@type table<PlayerID, table<integer, TerritoryStanding>>
         local playerterritories = {};
         for playerid, _ in pairs(game.Game.PlayingPlayers) do
@@ -28,15 +26,13 @@ function Server_StartGame(game, standing)
                 table.insert(playerterritories[stand.OwnerPlayerID], stand);
             end
         end
-        
+
         ---@type table<PlayerID, TerritoryID>
         local PlayerCapitals = {};
         for playerid, territories in pairs(playerterritories) do
-
-
             ---@type TerritoryStanding
             local chosenterritory = territories[math.random(#territories)]
-            
+
             ---@type table<EnumStructureType, integer>
             local structures = chosenterritory.Structures;
 
@@ -50,7 +46,7 @@ function Server_StartGame(game, standing)
             chosenterritory.Structures = structures;
 
             PlayerCapitals[playerid] = chosenterritory.ID;
-            
+
             ---@type table
 
             PlayerGameData[playerid].CapitalLocation = chosenterritory.ID;
@@ -62,8 +58,30 @@ function Server_StartGame(game, standing)
         PrivateGameData.PlayerCapitals = {};
     end
 
-    Mod.PlayerGameData = PlayerGameData;    
-    
+    Mod.PlayerGameData = PlayerGameData;
+
     Mod.PrivateGameData = PrivateGameData;
     SetOwnedCapitals(game, standing);
+
+    ---@type table<PlayerID, PlayerCards>
+    local cards = standing.Cards;
+    for playerid, _ in pairs(game.Game.PlayingPlayers) do
+        if cards[playerid] == nil then
+            cards[playerid] = WL.PlayerCards.Create(playerid);
+        end
+        ---@type ReinforcementCardInstance
+        local card = WL.ReinforcementCardInstance.Create(Mod.Settings.CapitalBonus);
+
+        ---@type table<CardInstanceID, CardInstance>
+        local wholecards = cards[playerid].WholeCards;
+        wholecards[card.ID] = card;
+        cards[playerid].WholeCards = wholecards;
+    end
+    standing.Cards = cards;
+
+    for playerid, _ in pairs(game.Game.PlayingPlayers) do
+        for cardinstanceid, cardinstance in pairs(standing.Cards[playerid].WholeCards) do
+            print(playerid, cardinstanceid, cardinstance.proxyType);
+        end
+    end
 end
